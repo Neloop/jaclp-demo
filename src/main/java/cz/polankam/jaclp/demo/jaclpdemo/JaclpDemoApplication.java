6f988e6bd13486026657bc2e4a2151f71b44b77b
Martin Polanka <PolankaMartin@gmail.com>
2020-03-07 11:37:00 +0100

    Initialize project with Spring Initializr

---
+++ src/main/java/cz/polankam/jaclp/demo/jaclpdemo/JaclpDemoApplication.java
@@ -0,0 +1,13 @@
+package cz.polankam.jaclp.demo.jaclpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaclpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaclpDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/polankam/jaclp/demo/jaclpdemo/JaclpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.polankam.jaclp.demo.jaclpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaclpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
